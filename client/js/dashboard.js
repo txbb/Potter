b75566f98272cbd09d17195ca8c0ca8446cd8eb3
zhangyang <px.zhangyang@gmail.com>
2015-10-22 17:17:51 +0800

    added fontawesome

--- client/js/dashboard.js
+++ client/js/dashboard.js
@@ -1,3 +1,14 @@
-Template.dashboard.onRendered(function(){
+var domWorkspace;
 
+function onScreenSizeChange() {
+
+}
+
+function init() {
+    domWorkspace.style.height = parseInt(window.innerHeight - 44) + 'px';
+}
+
+Template.dashboard.onRendered(function(){
+    domWorkspace = this.find('.workspace');
+    init();
 });
